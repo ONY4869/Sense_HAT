266283d45a7fd8f2dd684ac81046fe834c001183
ONY4869 <sheklamorny15@gmail.com>
2016-05-26 12:33:08 +0800

    temp
    The display board will change according to the temperature

---
+++ sensehat.py
@@ -0,0 +1,12 @@
+from sense_hat import SenseHat
+
+temperature = sense.temperature
+
+green = (0, 255, 0)
+white = (255, 255, 255)
+
+temp = 64 * temperature / 100
+
+pixels = [green if i < humidity_value else white for i in range(64)]
+
+sense.set_pixels(pixels)
