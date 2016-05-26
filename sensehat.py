34eda603642edfef406a1321cb690c98fe8b2e9f
ONY4869 <sheklamorny15@gmail.com>
2016-05-26 16:00:42 +0800

    Update sensehat.py

--- sensehat.py
+++ sensehat.py
@@ -8,6 +8,6 @@ white = (255, 255, 255)
 
 temp = 64 * temperature / 100
 
-pixels = [green if i < humidity_value else white for i in range(64)]
+pixels = [green if i < temp else white for i in range(64)]
 
 sense.set_pixels(pixels)
